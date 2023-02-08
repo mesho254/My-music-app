637a85ec1fdc909444542ecfa821d194e4115c72
Meshak Otieno <meshakotieno343@gmail.com>
2023-02-08 13:13:00 +0300

    initialized app using npm install vite@latest

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+    <h1 className="font-bold mt-2 text-2xl text-white">
+      Something went wrong, please try again...
+    </h1>
+  </div>
+);
+
+export default Error;
